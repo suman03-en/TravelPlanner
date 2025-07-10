ffe5083fbff931603dbdbcac5cec9c8bcffafcd0
suman phuyal <sfuyal59@gmail.com>
2025-07-10 22:21:03 +0545

    plan table creation sql updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import app from "./src/app.js";
 import dotenv from "dotenv";
 import { createTripTable } from "./src/models/tripModel.js";
 import { createUserTable } from "./src/models/userModel.js";
+import { createPlanTable } from "./src/models/planModel.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const port = process.env.PORT;
 const startServer = async() => {
   await createTripTable();
   await createUserTable();
+  await createPlanTable();
   app.listen(port, () => {
     console.log(`server running at ${port}`);
   });
--- backend/src/models/planModel.js
+++ backend/src/models/planModel.js
@@ -5,7 +5,7 @@ export const createPlanTable = async()=>{
     const query = `
     CREATE TABLE IF NOT EXISTS plans (
     plan_id SERIAL PRIMARY KEY,
-    trip_id INT NOT NULL REFERENCES trips(trips_id) ON DELETE CASCADE,
+    trip_id INT NOT NULL REFERENCES trips(trip_id) ON DELETE CASCADE,
     category VARCHAR(50) NOT NULL,
     budget_amount NUMERIC(10,2) NOT NULL
     );
